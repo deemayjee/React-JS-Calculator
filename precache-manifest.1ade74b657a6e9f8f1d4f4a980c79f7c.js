353fc8a4348332d6c906773dbccf06720f07fc12
System Administrator <root@Dimejis-MacBook-Pro.local>
2020-04-26 00:06:07 +0100

    Updates

---
+++ precache-manifest.1ade74b657a6e9f8f1d4f4a980c79f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff3d21f4bf9575743b7c29cbec5007a",
+    "url": "/React-JS-Calculator/index.html"
+  },
+  {
+    "revision": "11db2261e128a514f5cf",
+    "url": "/React-JS-Calculator/static/css/main.472ae113.chunk.css"
+  },
+  {
+    "revision": "dca94a2bb71dda4ee86f",
+    "url": "/React-JS-Calculator/static/js/2.4389c48b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-JS-Calculator/static/js/2.4389c48b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11db2261e128a514f5cf",
+    "url": "/React-JS-Calculator/static/js/main.6fa3bfb2.chunk.js"
+  },
+  {
+    "revision": "ca8258fb54adfacb7ef7",
+    "url": "/React-JS-Calculator/static/js/runtime-main.6d1bf106.js"
+  }
+]);
